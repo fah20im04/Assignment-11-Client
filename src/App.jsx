bc80a70994ff3dd3db733ba290f888a1c325a156
Fahim Ahmed Ayon <fa369383@gmail.com>
2025-12-06 18:43:10 +0600

    agfag

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-red-500 text-4xl'>Vite + React</h1>
+      <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
